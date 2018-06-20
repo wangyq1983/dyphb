df4c71cf1c67da93839eadfd909c66c881e65dc0
wangyuqiang <wangyuqiang@haima.me>
2018-06-20 13:38:06 +0800

    dyphb first

---
+++ app.js
@@ -0,0 +1,47 @@
+//app.js
+App({
+  onLaunch: function () {
+    // 展示本地存储能力
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              this.globalData.userInfo = res.userInfo
+
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
+            }
+          })
+        }
+      }
+    })
+  },
+  globalData: {
+    userInfo: null,
+
+    zhuboApi:{
+      dianzanApi:"http://api-dev.jiyoushe.cn/v2/circles/get_circle_list",
+      fensi:"http://api-dev.jiyoushe.cn/v2/rank/ranking_country_list",
+      pinglun:"http://api-dev.jiyoushe.cn/v2/amwaywall/get_list",
+      biaosheng:"http://api.jiyoushe.cn/v2/homepage/get_recommend_list"
+    },
+
+  }
+})
